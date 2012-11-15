5a8f7eea68a0114378a8bb24a5d6914d3315089b
asiance <asiance@asiancedev.asiance-dev.com>
2012-11-15 14:26:01 +0900

    launch file with forever

--- stage/bin/launch.sh
+++ stage/bin/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ls
 
 
 
-nodemon --watch ../views --watch ../routes --watch ../public --watch ../admin.js --watch ../public/stylesheets/ --watch ../login.js --watch ../app.js ../app.js
+forever app.js > output.log &
+# nodemon --watch ../views --watch ../routes --watch ../public --watch ../admin.js --watch ../public/stylesheets/ --watch ../login.js --watch ../app.js ../app.js
