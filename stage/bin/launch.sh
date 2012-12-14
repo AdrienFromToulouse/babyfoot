60428df0add0f7ee17d6b11e80b939dc0824754b
Adrien Desbiaux <adrien@asiance.com>
2012-12-14 16:29:31 +0900

    now output is in tmp folder

--- stage/bin/launch.sh
+++ stage/bin/launch.sh
@@ -3,5 +3,5 @@
 
 
 
-forever app.js > output.log &
+forever app.js > /tmp/output.log &
 # nodemon --watch ../views --watch ../routes --watch ../public --watch ../admin.js --watch ../public/stylesheets/ --watch ../login.js --watch ../app.js ../app.js
