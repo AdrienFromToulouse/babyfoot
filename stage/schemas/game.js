77c4efff681d870d27372039c0fd5a6d751bdd08
Adrien Desbiaux <adrien@asiance.com>
2013-06-17 16:32:29 +0900

    before switching to V3

--- stage/schemas/game.js
+++ stage/schemas/game.js
@@ -1,12 +1,12 @@
 var P1_ATTACKER = 0;
 var P2_ATTACKER = 1;
 var P3_ATTACKER = 2;
-var P4_ATTACKER = 3; 
+var P4_ATTACKER = 3;
 
 var P1_DEFENSER = 4;
 var P2_DEFENSER = 5;
 var P3_DEFENSER = 6;
-var P4_DEFENSER = 7; 
+var P4_DEFENSER = 7;
 
 /**
  * Include
@@ -16,28 +16,28 @@ var player = require('./player');
 
 
 var game_ctxt = {
-    "score_team1": "",
-    "score_team2": ""
+  "score_team1": "",
+  "score_team2": ""
 };
 
 
 /**
  * Define the schema
  */
-function createSchema(){
+function createSchema() {
 
-    var gameSchema = new mongoose.Schema({
+  var gameSchema = new mongoose.Schema({
 
-	created_at: Number,
-	started_at: Number,
-	stopped_at: Number,
-	duration: Number,
-	score_team1: Number,
-	score_team2: Number,
-	broadOnChan: Number,
+    created_at: Number,
+    started_at: Number,
+    stopped_at: Number,
+    duration: Number,
+    score_team1: Number,
+    score_team2: Number,
+    broadOnChan: Number,
 //	_players : [{ type: Schema.Types.ObjectId, ref: 'player' }]
-    });
-    return gameSchema;
+  });
+  return gameSchema;
 };
 
 
@@ -45,69 +45,77 @@ function createSchema(){
  * On start, get the new game ID and set it to each current players.
  *
  */
-exports.createNewGame = function(){
-    
-    console.log("CREATE GAME");
+exports.createNewGame = function () {
 
-    var gameSchema = createSchema();
+  console.log("CREATE GAME");
 
-    db = mongoose.createConnection('localhost', 'asiance_babyfoot');
+  var gameSchema = createSchema();
 
-    var Game = db.model('Game', gameSchema);
-   
-    var newgame = new Game;
+  db = mongoose.createConnection('localhost', 'asiance_babyfoot');
 
+  var Game = db.model('Game', gameSchema);
 
-    var playerSchema = player.create_Schema();
-    var Player = db.model('Player', playerSchema);
-
-    db.once('open', function (err, db) {
-
-	newgame.created_at = new Date().getTime();
-	newgame.started_at = new Date().getTime();
-	newgame.stopped_at = 0;
-	newgame.duration =   0;
-	newgame.score_team1 = 0;
-	newgame.score_team2 = 0;
-	newgame.broadOnChan = 1;
-
-	newgame.save(function (err) {
-    	    if(err){throw err;}
-
-	    var query = Game.find().sort({ created_at: 'desc'}).limit(1);
-
-	    query.exec(function (err, game) {
-	    	if (err) { throw err; }
+  var newgame = new Game;
 
 
-	    	/* get the last game ID created to retrieve data related to it */
-	    	game_id=game[game.length-1]._id;
+  var playerSchema = player.create_Schema();
+  var Player = db.model('Player', playerSchema);
 
-    	
-	    	//TODO replace the magic number by NBROF_PLAYERS
-   	    	Player.find().sort({ logged_at: 'desc'}).limit(4).exec(function (err, players) {
+  db.once('open', function (err, db) {
 
-	    	    if (err) { throw err; }
+    newgame.created_at = new Date().getTime();
+    newgame.started_at = new Date().getTime();
+    newgame.stopped_at = 0;
+    newgame.duration = 0;
+    newgame.score_team1 = 0;
+    newgame.score_team2 = 0;
+    newgame.broadOnChan = 1;
 
-	    	    console.log(game_id);
+    newgame.save(function (err) {
+      if (err) {
+        throw err;
+      }
 
-	    	    game_id = game_id.toString(16);
+      var query = Game.find().sort({ created_at: 'desc'}).limit(1);
 
-	    	        var player;
-	    	        var i;
-	    	        for (i = 0 ; i < players.length ; i++) {
-	    	    	player = players[i];
-	    	   
-	    	    	/* find si logged y a moins de 10 mins ou depend du nbrof player ???*/	
-	    	            player.update({ gameID : game_id},function (err) {
-	    	    	    if (err) { throw err; }
-	    	    	});
-	    	        }
-	    	        mongoose.disconnect();
-	    	});
-	    });
-	});
+      query.exec(function (err, game) {
+        if (err) {
+          throw err;
+        }
+
+
+        /* get the last game ID created to retrieve data related to it */
+        game_id = game[game.length - 1]._id;
+
+
+        //TODO replace the magic number by NBROF_PLAYERS
+        Player.find().sort({ logged_at: 'desc'}).limit(4).exec(function (err, players) {
+
+          if (err) {
+            throw err;
+          }
+
+          console.log(game_id);
+
+          game_id = game_id.toString(16);
+
+          var player;
+          var i;
+          for (i = 0; i < players.length; i++) {
+            player = players[i];
+
+            /* find si logged y a moins de 10 mins ou depend du nbrof player ???*/
+            player.update({ gameID: game_id}, function (err) {
+              if (err) {
+                throw err;
+              }
+            });
+          }
+          mongoose.disconnect();
+        });
+      });
     });
+  });
 };
 
 
@@ -115,71 +123,79 @@ exports.createNewGame = function(){
  * Set the stop date of a game
  *
  */
-exports.closeCurrentGame = function(){
-    
-    var gameSchema = createSchema();
+exports.closeCurrentGame = function () {
 
-    db = mongoose.createConnection('localhost', 'asiance_babyfoot');
+  var gameSchema = createSchema();
 
-    var Game = db.model('Game', gameSchema);
+  db = mongoose.createConnection('localhost', 'asiance_babyfoot');
 
- 
-    db.once('open', function (db,err) {
+  var Game = db.model('Game', gameSchema);
 
-	var query = Game.find().sort({ created_at: 'desc'}).limit(1);
 
-	query.exec(function (err, game) {
-	    if (err) { throw err; }
+  db.once('open', function (db, err) {
 
-	    /* get the last game ID created to retrieve data related to it */
+    var query = Game.find().sort({ created_at: 'desc'}).limit(1);
 
-	    var stopAt = new Date().getTime();
-	    var duration =  stopAt - game[game.length-1].started_at;
+    query.exec(function (err, game) {
+      if (err) {
+        throw err;
+      }
 
-	    //update stopped_at
-	    Game.update({ _id : game[game.length-1]._id}, { stopped_at : stopAt, duration: duration },function (err) {
-		if (err) { throw err; }
-		mongoose.disconnect();
-	    });
-	});
+      /* get the last game ID created to retrieve data related to it */
+
+      var stopAt = new Date().getTime();
+      var duration = stopAt - game[game.length - 1].started_at;
+
+      //update stopped_at
+      Game.update({ _id: game[game.length - 1]._id}, { stopped_at: stopAt, duration: duration }, function (err) {
+        if (err) {
+          throw err;
+        }
+        mongoose.disconnect();
+      });
     });
+  });
 };
 
 
 /**
- * Update the main score of a game: DOESN'T WORK REALY GOOD.... 
+ * Update the main score of a game: DOESN'T WORK REALY GOOD....
  *
  */
-exports.updateScore = function(message){
+exports.updateScore = function (message) {
 
-    var gameSchema = createSchema();
+  var gameSchema = createSchema();
 
-    db = mongoose.createConnection('localhost', 'asiance_babyfoot');
+  db = mongoose.createConnection('localhost', 'asiance_babyfoot');
 
-    var Game = db.model('Game', gameSchema);
+  var Game = db.model('Game', gameSchema);
 
 
-    score_team1 = message.score[P1_ATTACKER] + message.score[P1_DEFENSER] + message.score[P2_ATTACKER] + message.score[P2_DEFENSER];
-    score_team2 = message.score[P3_ATTACKER] + message.score[P3_DEFENSER] + message.score[P4_ATTACKER] + message.score[P4_DEFENSER];
-   
-    db.once('open', function () {
+  score_team1 = message.score[P1_ATTACKER] + message.score[P1_DEFENSER] + message.score[P2_ATTACKER] + message.score[P2_DEFENSER];
+  score_team2 = message.score[P3_ATTACKER] + message.score[P3_DEFENSER] + message.score[P4_ATTACKER] + message.score[P4_DEFENSER];
 
-	var query = Game.find().sort({ created_at: 'desc'}).limit(1);
+  db.once('open', function () {
 
-	query.exec(function (err, game) {
-	    if (err) { throw err; }
+    var query = Game.find().sort({ created_at: 'desc'}).limit(1);
 
-	    /* get the last game ID created to retrieve data related to it */
+    query.exec(function (err, game) {
+      if (err) {
+        throw err;
+      }
 
-	    console.log("GAME UPDATE !!!");
+      /* get the last game ID created to retrieve data related to it */
 
-	    console.log(game[game.length-1]._id);
+      console.log("GAME UPDATE !!!");
 
-	    Game.update({ _id : game[game.length-1]._id}, 
-			{ score_team1: score_team1 , score_team2: score_team2 },function (err) {
-			    if (err) { throw err; }
-			    mongoose.disconnect();
-			});
-	});
+      console.log(game[game.length - 1]._id);
+
+      Game.update({ _id: game[game.length - 1]._id},
+        { score_team1: score_team1, score_team2: score_team2 }, function (err) {
+          if (err) {
+            throw err;
+          }
+          mongoose.disconnect();
+        });
     });
+  });
 };
--- stage/schemas/player.js
+++ stage/schemas/player.js
@@ -10,128 +10,128 @@ var mongoose = require('mongoose');
  */
 var playerSchema = new mongoose.Schema({
 
-    personal:{
+  personal: {
 
-	fb_id: String,
- 	first_name: String,
-	last_name: String,
-	name: String,
-	gender: String,
-	locale: String,
-	link: String,
-	picture: String,
-	email: String
-    },
+    fb_id: String,
+    first_name: String,
+    last_name: String,
+    name: String,
+    gender: String,
+    locale: String,
+    link: String,
+    picture: String,
+    email: String
+  },
 
-    ready: { type: Boolean, default: true }, //page is already loaded so its true...
-    logged_at: Number,
-    babyId: Number,
-    gameID: String,
+  ready: { type: Boolean, default: true }, //page is already loaded so its true...
+  logged_at: Number,
+  babyId: Number,
+  gameID: String,
 
-    position: Number,
-    team: String,
+  position: Number,
+  team: String,
 
-    stats:{
-	score: Number,
-    }
+  stats: {
+    score: Number,
+  }
 });
 
 /**
  * Used to export the schema everywhere.
  *
  */
-getSchema = function(){
-    return playerSchema;
+getSchema = function () {
+  return playerSchema;
 };
 
 
 /**
- * Gathers all the ready players to send to the new player who is connecting. 
+ * Gathers all the ready players to send to the new player who is connecting.
  *
  * param[in]: bayeux - server to handle the websocket messaging.
  * param[in]: position - player position.
  * param[in]: babyId   - babyfoot identifier.
  *
  */
-exports.getCurrentPlayers = function(bayeux, position, babyId){
+exports.getCurrentPlayers = function (bayeux, position, babyId) {
 
-    var message = {
-    	"score" :   "",
-    	"name" :  "",
-	"picture": "",
-	"position": "",
-	"babyId": ""
-    };
+  var message = {
+    "score": "",
+    "name": "",
+    "picture": "",
+    "position": "",
+    "babyId": ""
+  };
 
-    db = mongoose.createConnection('localhost', 'asiance_babyfoot');
+  db = mongoose.createConnection('localhost', 'asiance_babyfoot');
 
-    var Player = db.model('Player', playerSchema);
-    var player = new Player;
-    
-    db.once('open', function () {
+  var Player = db.model('Player', playerSchema);
+  var player = new Player;
 
-	var query = Player.find({ ready: true }).limit(4);
-    	query.exec(function (err, players) {
+  db.once('open', function () {
 
-	    for(i = 0 ; i < players.length ; i++){
+    var query = Player.find({ ready: true }).limit(4);
+    query.exec(function (err, players) {
 
-		message.picture = players[i].personal.picture;
-		message.name = players[i].personal.first_name;
-		message.score = players[i].stats.score;
-		message.position = players[i].position;
-		message.babyId = players[i].babyId;
+      for (i = 0; i < players.length; i++) {
 
-		bayeux.getClient().publish('/player/'+position+'/baby/'+babyId, message);
-    	    }
-	    mongoose.disconnect();
-	});
+        message.picture = players[i].personal.picture;
+        message.name = players[i].personal.first_name;
+        message.score = players[i].stats.score;
+        message.position = players[i].position;
+        message.babyId = players[i].babyId;
+
+        bayeux.getClient().publish('/player/' + position + '/baby/' + babyId, message);
+      }
+      mongoose.disconnect();
     });
+  });
 };
 
 /**
- * Gathers all the ready players to send to the index on index connect. 
+ * Gathers all the ready players to send to the index on index connect.
  *
  * param[in]: bayeux - server to handle the websocket messaging.
  * param[in]: theScore - the score array.
  *
  */
-exports.getCurrentPlayersForIndex = function(bayeux, theScore){
+exports.getCurrentPlayersForIndex = function (bayeux, theScore) {
 
-    var message = [0,0,0,0];
+  var message = [0, 0, 0, 0];
 
-    db = mongoose.createConnection('localhost', 'asiance_babyfoot');
+  db = mongoose.createConnection('localhost', 'asiance_babyfoot');
 
-    var Player = db.model('Player', playerSchema);
-    var player = new Player;
-    
-    db.once('open', function () {
+  var Player = db.model('Player', playerSchema);
+  var player = new Player;
 
-	var query = Player.find({ ready: true }).limit(4);
-    	query.exec(function (err, players) {
+  db.once('open', function () {
 
-	    for(i = 0 ; i < players.length ; i++){
+    var query = Player.find({ ready: true }).limit(4);
+    query.exec(function (err, players) {
 
-		var msg = {
-    		    "score" :   "",
-    		    "name" :  "",
-		    "picture": "",
-		    "position": "",
-		    "babyId": ""
-		};
+      for (i = 0 ; i < players.length ; i++) {
 
-		msg.picture = '<img src="'+players[i].personal.picture+'">';
-		msg.name = players[i].personal.first_name;
-		//msg.score = players[i].stats.score;
- 		msg.position = players[i].position;
-		msg.babyId = players[i].babyId;
-		msg.score = theScore[msg.babyId - 1][msg.position - 1];
+        var msg = {
+          "score": "",
+          "name": "",
+          "picture": "",
+          "position": "",
+          "babyId": ""
+        };
 
-		message[msg.position - 1] = msg;
-    	    }
-	    mongoose.disconnect();
-	    bayeux.getClient().publish('/index', message);
-	});
+        msg.picture = '<img src="' + players[i].personal.picture + '">';
+        msg.name = players[i].personal.first_name;
+        //msg.score = players[i].stats.score;
+        msg.position = players[i].position;
+        msg.babyId = players[i].babyId;
+        msg.score = theScore[msg.babyId - 1][msg.position - 1];
+
+        message[msg.position - 1] = msg;
+      }
+      mongoose.disconnect();
+      bayeux.getClient().publish('/index', message);
     });
+  });
 };
 
 /**
@@ -140,48 +140,49 @@ exports.getCurrentPlayersForIndex = function(bayeux, theScore){
  * param[in]: player_logged - the player who is logging.
  *
  */
-exports.addPlayer = function(player_logged){
+exports.addPlayer = function (player_logged) {
 
-    db = mongoose.createConnection('localhost', 'asiance_babyfoot');
+  db = mongoose.createConnection('localhost', 'asiance_babyfoot');
 
-    var Player = db.model('Player', playerSchema);
-   
-    var player = new Player;
+  var Player = db.model('Player', playerSchema);
 
-    db.once('open', function (err, db) {
- 
-	player.personal.name = player_logged.name;
+  var player = new Player;
 
-	player.personal.fb_id = player_logged.fb_id;
-	player.personal.first_name = player_logged.first_name;
-	player.personal.last_name = player_logged.last_name;
-	player.personal.name = player_logged.name;
-	player.personal.gender = player_logged.gender;
-	player.personal.locale = player_logged.locale;
-	player.personal.link = player_logged.link;
-	player.personal.picture = player_logged.picture;
-	player.personal.email = player_logged.email; 
+  db.once('open', function (err, db) {
 
-	player.logged_at = new Date().getTime();
+    player.personal.name = player_logged.name;
 
-	player.babyId = player_logged.babyId;
-	player.position = player_logged.position;
+    player.personal.fb_id = player_logged.fb_id;
+    player.personal.first_name = player_logged.first_name;
+    player.personal.last_name = player_logged.last_name;
+    player.personal.name = player_logged.name;
+    player.personal.gender = player_logged.gender;
+    player.personal.locale = player_logged.locale;
+    player.personal.link = player_logged.link;
+    player.personal.picture = player_logged.picture;
+    player.personal.email = player_logged.email;
 
-	if( (player_logged.position == 1) || (player_logged.position == 2) ){
-	    player.team = 1;
-	}
-	else{
-	    player.team = 2;
-	}
-	player.stats.score = 0;
+    player.logged_at = new Date().getTime();
 
-	player.save(function (err) {
-    	    if(err){
-		mongoose.disconnect();
-    	    }else{
-		mongoose.disconnect();}
-	});
+    player.babyId = player_logged.babyId;
+    player.position = player_logged.position;
+
+    if ((player_logged.position == 1) || (player_logged.position == 2)) {
+      player.team = 1;
+    }
+    else {
+      player.team = 2;
+    }
+    player.stats.score = 0;
+
+    player.save(function (err) {
+      if (err) {
+        mongoose.disconnect();
+      } else {
+        mongoose.disconnect();
+      }
     });
+  });
 };
 
 /**
@@ -191,50 +192,50 @@ exports.addPlayer = function(player_logged){
  * param[in]: res - response.
  *
  */
-exports.getAplayer = function(req, res){
+exports.getAplayer = function (req, res) {
 
-    db = mongoose.createConnection('localhost', 'asiance_babyfoot');
+  db = mongoose.createConnection('localhost', 'asiance_babyfoot');
 
-    var Player = db.model('Player', playerSchema);
-    
-    db.once('open', function () {
+  var Player = db.model('Player', playerSchema);
 
-	var query = Player.findOne({position: req.body.position, 
-				    ready: true, 
-				    babyId: req.body.babyId, 
-				   });
+  db.once('open', function () {
 
-    	query.exec(function (err, player) {
-	    
-	    mongoose.disconnect();
-
-	    // res.header("Access-Control-Allow-Origin", "*"); 
-	    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	    res.send(player);
-	});
+    var query = Player.findOne({position: req.body.position,
+      ready: true,
+      babyId: req.body.babyId,
     });
+
+    query.exec(function (err, player) {
+
+      mongoose.disconnect();
+
+      // res.header("Access-Control-Allow-Origin", "*");
+      // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      res.send(player);
+    });
+  });
 }
 
-	
+
 /**
  * To switch from ready to notready when the player unsubscripts.
- * 
+ *
  * param[in]: babyId   - babyfoot identifier.
  * param[in]: position - player position.
  * param[in]: score    - player score.
- *    
+ *
  */
-exports.unsubscriptPlayer = function(babyId,
-				     position, 
-				     score){
+exports.unsubscriptPlayer = function (babyId, position, score) {
 
-    db = mongoose.createConnection('localhost', 'asiance_babyfoot');
+  db = mongoose.createConnection('localhost', 'asiance_babyfoot');
 
-    var Player = db.model('Player', playerSchema);
+  var Player = db.model('Player', playerSchema);
 
-    db.once('open', function () {
+  db.once('open', function () {
 
-	Player.update({position: position, babyId: babyId, ready: true},{"ready": false, stats: {score: score}},
-		      function(){mongoose.disconnect();});
-    });
+    Player.update({position: position, babyId: babyId, ready: true}, {"ready": false, stats: {score: score}},
+      function () {
+        mongoose.disconnect();
+      });
+  });
 };
