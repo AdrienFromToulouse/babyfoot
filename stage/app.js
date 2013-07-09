a25eac6d5ec2badb22679e3f2a0d7790a960ff43
Adrien Desbiaux <adrien@asiance.com>
2013-07-09 11:28:03 +0900

    clean + angularjs added

--- stage/app.js
+++ stage/app.js
@@ -1,53 +1,22 @@
-/**
- * ALL ON MOBILE
- *
- *
- */
-
-/**
- * due to the asynchrone stuffs it is impossible to
- * write the database each time a score is changed...
- * so its done on unsubscript of the player.
- */
-var score = [
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0]
-];
-
 var message = [0, 0, 0, 0];
-
-/**
- * To catch the webbrowser close event it is needed to fire
- * a callback on Disconnect event. This callback parses the clients
- * array and set to False the ready state if found.
- *
- */
-var clients = new Array(100);
 
 /**
  * Module dependencies.
  */
-
 var express = require('express')
   , routes = require('./routes')
-  , admin = require('./admin')
-  , login = require('./login')
   , http = require('http')
   , path = require('path')
   , faye = require('faye')
   , mongoose = require('mongoose')
   , game = require('./schemas/game')
+  , guest = require('./schemas/guest')
   , player = require('./schemas/player');
 
-
 /**
- * APP variable
- *
+ * Init the APP variable
  */
 var app = express();
-
 
 /**
  * Config
@@ -64,7 +33,6 @@ app.configure(function () {
   app.use(express.static(path.join(__dirname, 'public')));
   app.use(express.errorHandler({showStack: true, dumpExceptions: true}));
 });
-
 
 /**
  * Create Bayeux "server"
@@ -86,15 +54,11 @@ app.configure('development', function () {
    * General
    */
   app.get('/', function (req, res) {
-
     var ua = req.header('user-agent');
     if (/mobile/i.test(ua)) {
-
       res.render('index_mobile', { title: 'LiveGameUp!' });
-
     } else {
       res.render('index', { title: 'LiveGameUp!' });
-
     }
   });
 
@@ -102,125 +66,98 @@ app.configure('development', function () {
    *
    */
   app.get('/me/:fbid', function (req, res) {
-
     player.isPlaying(req, res);
   });
-
 
   /**
    * Login
    */
+  app.post('/login/guest', function (req, res) {
+    guest.getRandomGuest(res);
+  });
+
   app.get('/login', function (req, res) {
     res.render('login', { title: 'Login' })
   });
 
   app.post('/login', function(req, res) {
-
       var player_logged = req.body;
       player.addPlayer(player_logged);
 
       var msg = {
         "score": "",
-        "name": "",
+        "first_name": "",
         "picture": "",
         "position": "",
-        "babyId": ""
       };
 
-      msg.picture = '<img src="' + player_logged.picture + '">';
-      msg.name = player_logged.first_name;
-      msg.score = 0;
+      msg.picture = player_logged.picture;
+      msg.first_name = player_logged.first_name;
       msg.position = player_logged.position;
-      msg.babyId = player_logged.babyId;
+      msg.fb_id = player_logged.fb_id;
 
       message[player_logged.position - 1] = msg;
-
-      // update the global score
-      score[player_logged.babyId - 1][player_logged.position - 1] = 0;
 
       bayeux.getClient().publish('/admin', message);
       bayeux.getClient().publish('/index', message);
 
-      res.send(req.body); // looback object to client to get SUCCESS status
-
+      res.send(req.body);
   });
 
   /**
    * Admin
    */
-  app.get('/admin', admin.admin);
-
+  app.get('/admin', function (req, res) {
+    res.render('admin', { title: 'Admin' })
+  });
   /**
-   *
+   * 
    */
-  app.post('/player/setReady', function (req, res) {
-
-//    player.setReady(req, res);
+  app.post('/startStopGame', function (req, res) {
+      switch(req.body.data.cmd)
+      {
+      case "start":
+	  game.createNewGame();
+	  break;
+      case "stop":
+	  game.closeCurrentGame(req, res);
+	  break;
+      default:
+      }
   });
-  app.post('/player/setScores', function (req, res) {
 
-    player.setScores(score);
-    res.send(req.body);
+  app.post('/updateScore', function (req, res) {
+      game.updateScore(req, res);
   });
-
 });
 
 
 /**
  * Forwards message to the index page.
  */
-var subscription = bayeux.getClient().subscribe('/controller', function (game_ctxt) {
+var subscription = bayeux.getClient().subscribe('/controller', function (message) {
 
-  var msg = {
-    "score": "",
-    "name": "",
-    "picture": "",
-    "position": "",
-    "babyId": ""
-  };
-
-  msg.picture = game_ctxt.picture;
-  msg.name = game_ctxt.first_name;
-  msg.score = game_ctxt.score;
-  msg.position = game_ctxt.position;
-  msg.babyId = game_ctxt.babyId;
-
-  message[msg.position - 1] = msg;
-
-  score[0][game_ctxt.position - 1] = game_ctxt.score;
-
-  bayeux.getClient().publish('/index', message);
-
+    bayeux.getClient().publish('/index', message);
 });
-
 subscription.callback(function () {
   console.log('Subscription is now active!');
 });
-
 subscription.errback(function (error) {
   console.log(error.message);
 });
 
 
 bayeux.bind('subscribe', function (clientId, channel) {
-
-  if (channel == "/index") {
-
-    player.getCurrentPlayersForIndex(bayeux, score);
-
-  }
+    if (channel == "/index") {
+	game.getCurrentScoreNPlayer(bayeux);
+    }
 });
-
 bayeux.bind('unsubscribe', function (clientId, channel) {
 });
-
 bayeux.bind('publish', function (clientId, channel, data) {
 });
-
-
 bayeux.bind('disconnect', function (clientId) {
 });
-
 
 /**
  * Create server
